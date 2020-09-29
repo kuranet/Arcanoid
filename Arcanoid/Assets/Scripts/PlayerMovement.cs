40b142d3b52e349b83bd7e4408cb07b8b14fb397
Ann <anna.kurennaya38@gmail.com>
2020-09-29 09:18:06 +0300

    Add player movement

--- Arcanoid/Assets/Scripts/PlayerMovement.cs
+++ Arcanoid/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            Vector2 touchPosition = touch.position;
+            touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+
+            Vector2 target = new Vector2(touchPosition.x, transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }        
     }
 }
