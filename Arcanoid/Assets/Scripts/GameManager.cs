6051c7037c4134af9225f3ccba338ba50065ae9c
Ann <anna.kurennaya38@gmail.com>
2020-10-01 18:11:08 +0300

    Get closest point of collision

--- Arcanoid/Assets/Scripts/GameManager.cs
+++ Arcanoid/Assets/Scripts/GameManager.cs
@@ -45,27 +45,46 @@ public class GameManager : MonoBehaviour
             {
                 if(!obstacle.isCheckedForCurrentVelocity || obstacle.curretnVelocity != BallMovement.velocity)
                 {
-                    bool consist;
-                    Vector2 point;
-                    (consist, point) = LineCross.IsCrossed(obstacle.edgeCoords[0], obstacle.edgeCoords[1], ball.position, (Vector2)ball.position + BallMovement.velocity);
-                    if (consist)
-                        Debug.Log("COLLISION " + point.x + "   " + point.y);
-                    (consist, point) = LineCross.IsCrossed(obstacle.edgeCoords[1], obstacle.edgeCoords[2], ball.position, (Vector2)ball.position + BallMovement.velocity);
-                    if (consist)
-                        Debug.Log("COLLISION " + point.x + "   " + point.y);
-                    (consist, point) = LineCross.IsCrossed(obstacle.edgeCoords[2], obstacle.edgeCoords[3], ball.position, (Vector2)ball.position + BallMovement.velocity);
-                    if (consist)
-                        Debug.Log("COLLISION " + point.x + "   " + point.y);
-                    (consist, point) = LineCross.IsCrossed(obstacle.edgeCoords[3], obstacle.edgeCoords[0], ball.position, (Vector2)ball.position + BallMovement.velocity);
-                    if (consist)
-                        Debug.Log("COLLISION " + point.x + "   " + point.y);
+                    object[,] list = new object[4, 3]; 
+                    (list[0, 0], list[0, 1], list[0, 2]) = LineCross.IsCrossed(obstacle.edgeCoords[0], obstacle.edgeCoords[1], ball.position, (Vector2)ball.position + BallMovement.velocity);
+                    (list[1, 0], list[1, 1], list[1, 2]) = LineCross.IsCrossed(obstacle.edgeCoords[1], obstacle.edgeCoords[2], ball.position, (Vector2)ball.position + BallMovement.velocity);
+                    (list[2, 0], list[2, 1], list[2, 2]) = LineCross.IsCrossed(obstacle.edgeCoords[2], obstacle.edgeCoords[3], ball.position, (Vector2)ball.position + BallMovement.velocity);
+                    (list[3, 0], list[3, 1], list[3, 2]) = LineCross.IsCrossed(obstacle.edgeCoords[3], obstacle.edgeCoords[0], ball.position, (Vector2)ball.position + BallMovement.velocity);
+
+                    bool collides = false;
+                    Vector2 point = new Vector2();
+                    Vector2 normal = new Vector2();
+                    float distanceToCollision = 0;
+
+                    for(int i = 0; i < 4; i++)
+                    {
+                        if (!collides && (bool)list[i, 0])
+                        {
+                            collides = true;
+                            point = (Vector2)list[i, 1];
+                            normal = (Vector2)list[i, 2];
+                            distanceToCollision = Vector2.Distance(ball.position, point);
+                        }
+                        else if(collides && (bool)list[i, 0])
+                        {
+
+                            float tempDistane = Vector2.Distance(ball.position, (Vector2)list[i, 1]);
+                            if (tempDistane < distanceToCollision)
+                            {
+                                point = (Vector2)list[i, 1];
+                                normal = (Vector2)list[i, 2];
+                                distanceToCollision = Vector2.Distance(ball.position, point);
+                            }
+                        }
+                    }
+                    if (collides)
+                        Debug.Log("POINT OF COLLISION " + point.x + "  " + point.y);
 
                     Debug.Log("Can be collision with " + obstacle.position.x + "   " + obstacle.position.y);
                     obstacle.curretnVelocity = BallMovement.velocity;
                     obstacle.isCheckedForCurrentVelocity = true;
                 }
             }
-
         }
         float distanceE = Vector2.Distance(player.position, ball.position);
         float collisionRadius = Mathf.Sqrt( Mathf.Pow( player.localScale.x /2,2) + Mathf.Pow((player.localScale.y / 2) , 2));
@@ -73,8 +92,6 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("Can be collision with " + player.name);
         }
-    
-    
     }
 
     public static event ActionHandler BallOutOfScreen;
@@ -85,11 +102,6 @@ public class GameManager : MonoBehaviour
         BallMovement.velocity = new Vector2(0, 1);
         BallMovement.isMoving = true;
         ballRadius = transform.localScale.x / 2;
-
-        //foreach(var obstacle in LevelGenerator.obstacles)
-        //{
-        //    obstacle.curretnVelocity = BallMovement.velocity;
-        //}
     }
 
 
--- Arcanoid/Assets/Scripts/LineCross.cs
+++ Arcanoid/Assets/Scripts/LineCross.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LineCross : MonoBehaviour
 {
-    public static (bool,Vector2) IsCrossed(Vector2 start, Vector2 end, Vector2 positionCurrent, Vector2 positionNext)
+    public static (bool,Vector2,Vector2) IsCrossed(Vector2 start, Vector2 end, Vector2 positionCurrent, Vector2 positionNext)
     {
         /// (Ye - Ys) * X - (Xe - Xs) * Y - (Ye - Ys) * Xe  + (Xe - Xs) * Ye = 0
         /// (Yn - Yc) * X - (Xn - Xc) * Y - (Yn - Yc) * Xn  + (Xn - Xc) * Yn = 0
@@ -21,6 +21,7 @@ public class LineCross : MonoBehaviour
 
             bool consist = false;
             Vector2 collisionPoint = new Vector2();
+            Vector2 normal = new Vector2();
 
             if ((X <= end.x && X >= start.x) || (X >= end.x && X <= start.x))
             {
@@ -28,11 +29,12 @@ public class LineCross : MonoBehaviour
                 {
                     consist = true;
                     collisionPoint = new Vector2(X, Y);
+                    normal = Quaternion.Euler(0, 0, 90) * (end - start);
                 }
             }
 
-            return (consist, collisionPoint);
+            return (consist, collisionPoint,normal);
         }
-        else return (false, new Vector2()); 
+        else return (false, new Vector2(), new Vector2()); 
     }
 }
